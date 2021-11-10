0f600592e8814145b3e49105f1a992a260b43df5
Parker <parkerlockhart42@gmail.com>
2021-11-10 14:11:39 -0600

    Test: adult? method

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -3,6 +3,7 @@ require './lib/passenger'
 RSpec.describe Passenger do
   before(:each) do
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   describe '#initialize' do
@@ -18,4 +19,11 @@ RSpec.describe Passenger do
       expect(@charlie.age).to eq(18)
     end
   end
+
+  describe '#adult?' do
+    it 'returns whether passenger age >= 18' do
+      expect(@charlie.adult?).to eq(true)
+      expect(@taylor.adult?).to eq(false)
+    end
+  end 
 end
