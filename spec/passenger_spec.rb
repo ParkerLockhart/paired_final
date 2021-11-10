6cda1264781e133f60d55af14c06210854241d24
Parker <parkerlockhart42@gmail.com>
2021-11-10 08:07:17 -0600

    test: passenger class initialize

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -1,1 +1,21 @@
 require './lib/passenger'
+
+Rspec.describe Passenger do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@charlie).to be_instance_of(Charlie)
+    end
+
+    it 'has a name' do
+      expect(@charlie.name).to eq("Charlie")
+    end
+
+    it 'has an age' do
+      expect(@charlie.age).to eq(18)
+    end
+  end
+end 
