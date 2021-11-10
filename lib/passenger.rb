6ae81cf0c39407220d6d37c9b7c963c2ebedf509
Parker <parkerlockhart42@gmail.com>
2021-11-10 14:56:02 -0600

    Add driver? and drive methods

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,12 +1,21 @@
 class Passenger
-  attr_reader :name, :age
+  attr_reader :name, :age, :driver
 
   def initialize(info)
     @name = info["name"]
     @age = info["age"]
+    @driver = false
   end
 
   def adult?
     age >= 18
-  end 
+  end
+
+  def drive
+    @driver = true
+  end
+
+  def driver?
+    @driver
+  end
 end
