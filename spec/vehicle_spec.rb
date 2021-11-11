8397df556417c23b16827d0701ce2db34a00a51e
Parker <parkerlockhart42@gmail.com>
2021-11-10 19:54:24 -0600

    Test: passengers attribute, add_passenger method

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -4,6 +4,9 @@ require './lib/vehicle'
 RSpec.describe Vehicle do
   before(:each) do
     @vehicle = Vehicle.new("2001", "Honda", "Civic")
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @jude = Passenger.new({"name" => "Jude", "age" => 20})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
   end
 
   describe '#initialize' do
@@ -22,6 +25,10 @@ RSpec.describe Vehicle do
     it 'has a model' do
       expect(@vehicle.model).to eq("Civic")
     end
+
+    it 'has passengers' do
+      expect(@vehicle.passengers).to eq([])
+    end
   end
 
   describe '#speeding?' do
@@ -29,6 +36,15 @@ RSpec.describe Vehicle do
       expect(@vehicle.speeding?).to eq(false)
       @vehicle.speed
       expect(@vehicle.speeding?).to eq(true)
+    end
+  end
+
+  describe '#add_passenger' do
+    it 'adds passenger instance to passengers attribute' do
+      @vehicle.add_passenger(@charlie)
+      @vehicle.add_passenger(@jude)
+      @vehicle.add_passenger(@taylor)
+      expect(@vehicle.passengers).to eq([@charlie, @jude, @taylor])
     end
   end 
 end
