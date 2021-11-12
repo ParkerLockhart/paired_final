58eb35fe8d0829127aaff6ac7fda778cd6231c05
Parker <parkerlockhart42@gmail.com>
2021-11-12 11:28:14 -0600

    Add admit vehicle and admit passengers methods

--- lib/park.rb
+++ lib/park.rb
@@ -2,10 +2,26 @@ require './lib/passenger'
 require './lib/vehicle'
 
 class Park
-  attr_reader :name, :price
+  attr_reader :name, :price, :vehicles_admitted, :passengers
 
   def initialize(name, price)
     @name = name
     @price = price
+    @vehicles_admitted = []
+    @passengers = []
   end
+
+  def admit_vehicle(vehicle)
+    @vehicles_admitted << vehicle
+  end
+
+  def admit_passengers
+    @vehicles_admitted.each do |vehicle|
+      vehicle.passengers.each do |passenger|
+        @passengers << passenger
+      end  
+    end
+  end
+
+
 end
