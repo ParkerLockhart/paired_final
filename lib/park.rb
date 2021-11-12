b784c002599253b975945da5cc534791db4cd296
Parker <parkerlockhart42@gmail.com>
2021-11-12 11:33:33 -0600

    Add charge_admission method, total_revenue attribute

--- lib/park.rb
+++ lib/park.rb
@@ -2,13 +2,14 @@ require './lib/passenger'
 require './lib/vehicle'
 
 class Park
-  attr_reader :name, :price, :vehicles_admitted, :passengers
+  attr_reader :name, :price, :vehicles_admitted, :passengers, :total_revenue
 
   def initialize(name, price)
     @name = name
     @price = price
     @vehicles_admitted = []
     @passengers = []
+    @total_revenue = 0
   end
 
   def admit_vehicle(vehicle)
@@ -19,9 +20,17 @@ class Park
     @vehicles_admitted.each do |vehicle|
       vehicle.passengers.each do |passenger|
         @passengers << passenger
-      end  
+      end
     end
   end
+
+  def charge_admission
+    @passengers.each do |passenger|
+      if passenger.adult?
+        @total_revenue += @price
+      end
+    end
+  end 
 
 
 end
