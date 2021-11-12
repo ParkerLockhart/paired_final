3253234a6b22424c8fc9823469d896e687e3c9fa
Parker <parkerlockhart42@gmail.com>
2021-11-12 11:38:11 -0600

    Test: add passengers to vehicle1

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,6 +10,10 @@ describe Park do
     @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
     @jude = Passenger.new({"name" => "Jude", "age" => 20})
     @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    @parker = Passenger.new({"name" => "Parker", "age" => 34})
+    @sam = Passenger.new({"name" => "Sam", "age" => 11})
+    @vehicle1.add_passenger(@parker)
+    @vehicle1.add_passenger(@sam)
     @vehicle.add_passenger(@charlie)
     @vehicle.add_passenger(@jude)
     @vehicle.add_passenger(@taylor)
@@ -31,6 +35,14 @@ describe Park do
     it 'has vehicles_admitted' do
       expect(@garner.vehicles_admitted).to eq([])
     end
+
+    it 'has passengers' do
+      expect(@garner.passengers).to eq([])
+    end
+
+    it 'has total_revenue' do
+      expect(@garner.total_revenue).to eq(0)
+    end
   end
 
   describe '#admit_vehicle' do
@@ -47,17 +59,17 @@ describe Park do
       @garner.admit_vehicle(@vehicle)
       @garner.admit_vehicle(@vehicle1)
       @garner.admit_passengers
-      expect(@garner.passengers).to eq([@charlie, @jude, @taylor])
+      expect(@garner.passengers).to eq([@charlie, @jude, @taylor, @parker, @sam])
     end
   end
 
   describe '#charge_admission' do
-    it 'charges each adult admission price' do  
+    it 'charges each adult admission price' do
       @garner.admit_vehicle(@vehicle)
       @garner.admit_vehicle(@vehicle1)
       @garner.admit_passengers
       @garner.charge_admission
-      expect(@garner.total_revenue).to eq(12)
+      expect(@garner.total_revenue).to eq(18)
     end
   end
 
